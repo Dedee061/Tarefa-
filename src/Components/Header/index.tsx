feb4e1a57270da2f2af60aad0b28caf82f1c4ca9
Andre Santos <andresantoss0598@gmail.com>
2024-10-03 10:59:22 -0300

    criado o header e configurando a pagina de home

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "../Header/styles.module.css";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <section className={styles.content}>
+        <nav className={styles.nav}>
+          <Link href={"/"}>
+            <h1 className={styles.logo}>
+              Tarefas
+              <span>+</span>
+            </h1>
+          </Link>
+          <Link className={styles.link} href={'/dashboard'}>
+            Meu Painel
+          </Link>
+        </nav>
+
+        <button className={styles.loginButton}>Fazer Login</button>
+      </section>
+    </header>
+  );
+}
