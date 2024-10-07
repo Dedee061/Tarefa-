a36cfbece590ecae1601cc89f413a84b6f85a14b
Andre Santos <andresantoss0598@gmail.com>
2024-10-07 20:23:42 -0300

    configurando o next auth

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+
+export default function dashboard() {
+  return (
+    <>
+        <title>Meu Painel de tarefas</title>
+    <div className={styles.container}>
+    
+    </div>
+    </>
+  )
+}
