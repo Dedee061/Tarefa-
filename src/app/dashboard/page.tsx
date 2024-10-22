e09a2435150bdf5df22052de545f9fdd2e7f9dff
Andre Santos <andresantoss0598@gmail.com>
2024-10-22 11:31:58 -0300

    finalizando tela de dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,8 @@ import { getSession, useSession } from 'next-auth/react'
 import { redirect } from 'next/navigation'
 import Loading from '../../Components/Loading/index.'
 import { TextArea } from '../../Components/TextArea'
+import { FaShare, FaTrash } from 'react-icons/fa'
+import { FiShare2 } from 'react-icons/fi'
 
 export default function dashboard() {
 
@@ -34,7 +36,7 @@ export default function dashboard() {
             <div className={styles.contentForm}>
               <h1 className={styles.title}>Qual sua Tarefa ?</h1>
               <form>
-                <TextArea placeholder='Digite qual sua terafa'/>
+                <TextArea placeholder='Digite qual sua terafa' />
                 <div className={styles.checkboxarea}>
                   <input type="checkbox" className={styles.checkbox} />
                   <label htmlFor="">Deixa Tarefa Publica</label>
@@ -42,6 +44,25 @@ export default function dashboard() {
                 <button type='submit' className={styles.button}>Enviar</button>
               </form>
             </div>
+          </section>
+
+          <section className={styles.teskContainer}>
+            <h1>Minhas Tarefas</h1>
+
+            <article className={styles.task}>
+              <div className={styles.tagContainer}>
+                <label className={styles.tag}>PUBLICO</label>
+                <button className={styles.shareButton}>
+                  <FiShare2 size={22} color='#896BFF' />
+                </button>
+              </div>
+              <div className={styles.taskContent}>
+                <p>Minha tarefa de exemplo</p>
+                  <button className={styles.trashButton}>
+                    <FaTrash size={24} color='#ea3140'/>
+                  </button>
+                </div>
+            </article>
           </section>
         </main>
       </div>
