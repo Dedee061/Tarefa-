689ba09f1f37b43bc5d0b9b623b7ae3dd46d3d99
Andre Santos <andresantoss0598@gmail.com>
2024-10-23 18:20:30 -0300

    coletando input

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect } from 'react'
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react'
 import styles from './styles.module.css'
 import { getSession, useSession } from 'next-auth/react'
 import { redirect } from 'next/navigation'
@@ -9,8 +9,23 @@ import { FaShare, FaTrash } from 'react-icons/fa'
 import { FiShare2 } from 'react-icons/fi'
 
 export default function dashboard() {
+  const [input, setInput] = useState('')
+  const [publictask, setPublicTask] = useState(false)
+
 
   const { data: session, status } = useSession();
+
+  function handlerChangePublic(event: ChangeEvent<HTMLInputElement>) {
+      
+      setPublicTask(event.target.checked)
+    }
+
+    function handlerRegisterTask(event: FormEvent) {
+      event.preventDefault();
+      if(input == '') return;
+
+      alert('teste')
+    }
 
   useEffect(() => {
     if (status === "loading") {
@@ -35,10 +50,19 @@ export default function dashboard() {
           <section className={styles.content}>
             <div className={styles.contentForm}>
               <h1 className={styles.title}>Qual sua Tarefa ?</h1>
-              <form>
-                <TextArea placeholder='Digite qual sua terafa' />
+              <form onSubmit={handlerRegisterTask}>
+                <TextArea
+                  value={input}
+                  onChange={(event: ChangeEvent<HTMLTextAreaElement>) => setInput(event.target.value)}
+                  placeholder='Digite qual sua terafa'
+                />
                 <div className={styles.checkboxarea}>
-                  <input type="checkbox" className={styles.checkbox} />
+                  <input 
+                  type="checkbox" 
+                  className={styles.checkbox}
+                  checked={publictask} 
+                  onChange={handlerChangePublic}
+                  />
                   <label htmlFor="">Deixa Tarefa Publica</label>
                 </div>
                 <button type='submit' className={styles.button}>Enviar</button>
@@ -58,10 +82,10 @@ export default function dashboard() {
               </div>
               <div className={styles.taskContent}>
                 <p>Minha tarefa de exemplo</p>
-                  <button className={styles.trashButton}>
-                    <FaTrash size={24} color='#ea3140'/>
-                  </button>
-                </div>
+                <button className={styles.trashButton}>
+                  <FaTrash size={24} color='#ea3140' />
+                </button>
+              </div>
             </article>
           </section>
         </main>
