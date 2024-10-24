cd182e61016d0ada26e8d15acbb6247feea4484c
Andre Santos <andresantoss0598@gmail.com>
2024-10-24 19:16:55 -0300

    cadastrando tarefas no banco de dados

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,31 +1,61 @@
-"use client"
-import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react'
-import styles from './styles.module.css'
-import { getSession, useSession } from 'next-auth/react'
-import { redirect } from 'next/navigation'
-import Loading from '../../Components/Loading/index.'
-import { TextArea } from '../../Components/TextArea'
-import { FaShare, FaTrash } from 'react-icons/fa'
-import { FiShare2 } from 'react-icons/fi'
+"use client";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import styles from "./styles.module.css";
+import { getSession, useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import Loading from "../../Components/Loading/index.";
+import { TextArea } from "../../Components/TextArea";
+import { FaShare, FaTrash } from "react-icons/fa";
+import { FiShare2 } from "react-icons/fi";
+
+import { db } from "../services/firebaseConnection";
+import {
+  addDoc,
+  collection,
+  query,
+  orderBy,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
 
 export default function dashboard() {
-  const [input, setInput] = useState('')
-  const [publictask, setPublicTask] = useState(false)
-
+  const [input, setInput] = useState("");
+  const [publictask, setPublicTask] = useState(false);
 
   const { data: session, status } = useSession();
 
   function handlerChangePublic(event: ChangeEvent<HTMLInputElement>) {
-      
-      setPublicTask(event.target.checked)
-    }
+    setPublicTask(event.target.checked);
+  }
 
-    function handlerRegisterTask(event: FormEvent) {
-      event.preventDefault();
-      if(input == '') return;
+  async function handlerRegisterTask(event: FormEvent) {
+    event.preventDefault();
+    if (input == "") return;
 
-      alert('teste')
+    try {
+      await addDoc(collection(db, "task"), {
+        tarefa: input,
+        created: new Date(),
+        user: session?.user?.email,
+        public: publictask,
+      });
+
+      setInput(""), setPublicTask(false);
+    } catch (err) {
+      console.error("Error adding document: ", err);
     }
+  }
+
+  useEffect(() => {
+    async function loadTask() {
+      const taskRef = collection(db, "task");
+      const q = query(
+        taskRef,
+        orderBy("created", "desc"),
+        where("user", "==", session?.user?.email)
+      )
+    }
+  })
 
   useEffect(() => {
     if (status === "loading") {
@@ -33,13 +63,12 @@ export default function dashboard() {
     }
 
     if (!session) {
-
-      redirect('/')
+      redirect("/");
     }
   }, [session, status]);
 
   if (status === "loading") {
-    return <Loading />
+    return <Loading />;
   }
 
   return (
@@ -53,19 +82,23 @@ export default function dashboard() {
               <form onSubmit={handlerRegisterTask}>
                 <TextArea
                   value={input}
-                  onChange={(event: ChangeEvent<HTMLTextAreaElement>) => setInput(event.target.value)}
-                  placeholder='Digite qual sua terafa'
+                  onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
+                    setInput(event.target.value)
+                  }
+                  placeholder="Digite qual sua terafa"
                 />
                 <div className={styles.checkboxarea}>
-                  <input 
-                  type="checkbox" 
-                  className={styles.checkbox}
-                  checked={publictask} 
-                  onChange={handlerChangePublic}
+                  <input
+                    type="checkbox"
+                    className={styles.checkbox}
+                    checked={publictask}
+                    onChange={handlerChangePublic}
                   />
                   <label htmlFor="">Deixa Tarefa Publica</label>
                 </div>
-                <button type='submit' className={styles.button}>Enviar</button>
+                <button type="submit" className={styles.button}>
+                  Enviar
+                </button>
               </form>
             </div>
           </section>
@@ -77,13 +110,13 @@ export default function dashboard() {
               <div className={styles.tagContainer}>
                 <label className={styles.tag}>PUBLICO</label>
                 <button className={styles.shareButton}>
-                  <FiShare2 size={22} color='#896BFF' />
+                  <FiShare2 size={22} color="#896BFF" />
                 </button>
               </div>
               <div className={styles.taskContent}>
                 <p>Minha tarefa de exemplo</p>
                 <button className={styles.trashButton}>
-                  <FaTrash size={24} color='#ea3140' />
+                  <FaTrash size={24} color="#ea3140" />
                 </button>
               </div>
             </article>
