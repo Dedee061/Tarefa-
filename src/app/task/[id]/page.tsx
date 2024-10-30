95c2a1226175a1cda85301b701884b2d16801b3d
Andre Santos <andresantoss0598@gmail.com>
2024-10-30 11:05:11 -0300

    criando roteamento de detalhes

---
+++ src/app/task/[id]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+export default function Task() {
+  return (
+    <>
+    <div className={styles.container}>  
+    <title>Detalhes da Tarefas</title>
+        <main className={styles.main}>
+            <h1>Tarefas</h1>
+        </main>
+
+    </div>
+    </>
+  )
+}
